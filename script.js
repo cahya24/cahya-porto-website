22987af1e30b49d5f62be059dccc927971279aa2
cahya24 <cahya.butex@yahoo.com>
2024-08-19 13:16:02 +0700

    First Iteration Home Page + Navbar Mockup

---
+++ script.js
@@ -0,0 +1,27 @@
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const sections = document.querySelectorAll('section');
+
+const options = {
+    threshold: 0.25
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        }
+    });
+}, options);
+
+sections.forEach(section => {
+    observer.observe(section);
+});
